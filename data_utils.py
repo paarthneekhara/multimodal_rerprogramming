79b36766ea309bf2026f8739cb6427468bf9bc21
Paarth Neekhara <paarth.n@gmail.com>
2020-11-02 14:45:10 -0800

    added more datasets

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,17 @@
 dataset_sentence_key_mapping = {
     'imdb' : 'text',
-    'emotion' : 'text'
+    'emotion' : 'text',
+    'ag_news' : 'text',
+    'emo' : 'text',
+    'yelp_polarity' : 'text'
+}
+
+dataset_num_classes = {
+    'imdb' : 2,
+    'emotion' : 6,
+    'ag_news' : 4,
+    'emo' : 4,
+    'yelp_polarity' : 2
 }
 
 def label_mapper(e, text_dataset):
@@ -23,7 +34,3 @@ def label_mapper(e, text_dataset):
 
     return e
 
-dataset_num_classes = {
-    'imdb' : 2,
-    'emotion' : 6
-}
