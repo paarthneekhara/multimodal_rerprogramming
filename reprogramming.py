21ebfe77f886289cddb01bda8a048368fd93e11f
Paarth Neekhara <paarth.n@gmail.com>
2020-11-11 22:27:46 -0800

    moving model code to a new file cause I am bored

--- reprogramming.py
+++ reprogramming.py
@@ -5,16 +5,13 @@ import torch.nn as nn
 import torch
 import timm
 import torch.optim as optim
-import torch.nn.functional as F
 from tensorboardX import SummaryWriter
 import os
 import data_utils
-from torchvision import transforms
-from PIL import Image
-from torchvision.transforms import ToTensor, Resize, Normalize
 import json
 import numpy as np
 import pprint
+import reprogramming_model
 
 train_hps = {
     'num_epochs' : 100,
@@ -90,59 +87,6 @@ def get_imagenet_label_list(vision_model, base_image, img_size):
     label_list = label_sort.detach().cpu().numpy().tolist()
 
     return label_list
-
-
-class ReprogrammingFuntion(nn.Module):
-    def __init__(self, vocab_size, img_patch_size = 16, img_size = 384, 
-        img_path=None, alpha=0.2, 
-        img_mean=(0.5, 0.5, 0.5), img_std=(0.5, 0.5, 0.5)):
-        super(ReprogrammingFuntion, self).__init__()
-
-        assert img_size % img_patch_size == 0
-        self.img_patch_size = img_patch_size
-        self.img_size = img_size
-        self.token_embedding = nn.Embedding(vocab_size, img_patch_size * img_patch_size * 3)
-        self.num_patches_row = int(img_size/img_patch_size)
-        self.num_patches = self.num_patches_row * self.num_patches_row
-        self.base_image = None
-        if img_path is not None:
-            image = Image.open(img_path)
-            transform=transforms.Compose([
-                                  Resize((img_size, img_size)),
-                                  ToTensor(),
-                                Normalize(img_mean,img_std),
-                                ])
-
-            image = transform(image) 
-            self.base_image = torch.tensor(image, requires_grad=False).to(device)
-            self.alpha = alpha
-
-
-    def forward(self, sentence_batch):
-        sentence_embedding = F.tanh(self.token_embedding(sentence_batch)) # (N, l, 16*16*3)
-        _N, _L, _ = sentence_embedding.size()
-        sentence_embedding = sentence_embedding.view(_N, _L, 3, self.img_patch_size, self.img_patch_size)
-
-        reprogrammed_image = torch.zeros(_N, 3, self.img_size, self.img_size).to(device)
-        
-        for patch_idx in range(self.num_patches):
-            i_start = int(patch_idx / self.num_patches_row) * self.img_patch_size
-            j_start = (patch_idx % self.num_patches_row) * self.img_patch_size
-            i_end = i_start + self.img_patch_size
-            j_end = j_start + self.img_patch_size
-            if patch_idx < _L:
-                reprogrammed_image[:,:,i_start:i_end,j_start:j_end] = sentence_embedding[:,patch_idx]
-            else:
-                # adding the padding embedding all the way till the end
-                reprogrammed_image[:,:,i_start:i_end,j_start:j_end] = sentence_embedding[:,_L-1]
-
-        if self.base_image is not None:
-            base_image_batch = self.base_image[None].repeat((_N, 1, 1, 1))
-            reprogrammed_image = base_image_batch + self.alpha * reprogrammed_image
-        
-        reprogrammed_image = torch.clamp(reprogrammed_image, -1.0, 1.0) # because image is normalized
-        
-        return reprogrammed_image
 
 
 def evaluate(dataloader, vision_model, reprogrammer, tb_writer, 
@@ -266,10 +210,11 @@ def main():
     n_classes = data_utils.dataset_num_classes[args.text_dataset]
     
 
-    reprogrammer = ReprogrammingFuntion(vocab_size, args.img_patch_size, 
+    reprogrammer = reprogramming_model.ReprogrammingFuntion(vocab_size, args.img_patch_size, 
         args.img_size, 
         img_path = args.base_image_path, alpha = args.pert_alpha, img_mean = img_mean, img_std = img_std)
     reprogrammer.to(device)
+    
     image_net_labels = get_imagenet_label_list(vision_model, reprogrammer.base_image, args.img_size)
     print("Imagenet Label Ordering..", image_net_labels[:20])
     optimizer = optim.Adam(reprogrammer.parameters(), lr=train_hps['lr'])
