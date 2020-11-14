18431bd615022ca5471db9259a5aebdef830ccd1
Paarth Neekhara <paarth.n@gmail.com>
2020-11-13 20:10:38 -0800

    cleaned up the code, added logdir saving

--- CodeForBaselinePY.py
+++ CodeForBaselinePY.py
@@ -3,17 +3,14 @@
 # jid020@ucsd.edu
 
 # Sample commands to use:
-#   python CodeForBaselinePY.py 
+#   python train_baseline_classifier.py 
 #       Everything is in default
-#   python CodeForBaselinePY.py --text_dataset 'yelp_polarity' --Algorithm 1
+#   python train_baseline_classifier.py --text_dataset 'yelp_polarity' --Algorithm 1
 #       Choose algorithm or dataset you wish 
 # Unfinished:
 #   check comment code to utilize the tensorboard and record the result as you wish
 ########################
-
-
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import SGDClassifier
 from sklearn.linear_model import LogisticRegression
@@ -30,9 +27,6 @@ import data_utils
 import os
 # from tensorboardX import SummaryWriter
 
-
-MLModelFiles = '' 
-
 dataset_sentence_key_mapping = {
     'imdb' : 'text',
     'emotion' : 'text',
@@ -42,12 +36,12 @@ dataset_sentence_key_mapping = {
 }
 
 
-def save_obj(obj, name ):
-    with open(name + '.pkl', 'wb') as f:
+def save_obj(obj, file_path ):
+    with open(file_path, 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-def load_obj(name ):
-    with open(name + '.pkl', 'rb') as f:
+def load_obj(file_path):
+    with open(file_path, 'rb') as f:
         return pickle.load(f)
 
 
@@ -55,23 +49,22 @@ def main():
     p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('--text_dataset', type=str , default = 'emotion', 
         help='Enter one of following dataset: imdb, emotion, ag_news, emo, yelp_polarity')
-    p.add_argument('--Algorithm', type=int, default = 3, 
-        help='Enter algorithm number 1 ~ 4: LogisticRegression, Perceptron, LinearSVC, SGDClassifier')
-    # p.add_argument('--logdir', type=str, default = "/data2/paarth/ReprogrammingTransformers/ClassificationModels")
+    p.add_argument('--algorithm', type=str, default = "sgd", 
+        choices = ["logistic", "perceptron", "svc", "sgd"]
+    )
+    p.add_argument('--logdir', type=str, default = "/data2/paarth/ReprogrammingTransformers/BaselineClassificationModels")
     p.add_argument('--cache_dir', type=str, default = "/data2/paarth/HuggingFaceDatasets")
 
     args = p.parse_args()
     assert args.text_dataset in dataset_sentence_key_mapping
-    assert args.Algorithm in list(range(1, 5))
+    
 
 
-    # exp_name = "classifier_{}_model_{}".format(args.text_dataset, args.Algorithm)
+    exp_name = "baseline_{}_model_{}".format(args.text_dataset, args.algorithm)
 
-    # logdir = os.path.join(args.logdir, exp_name)
-    # if not os.path.exists(logdir):
-    #     os.makedirs(logdir)
-
-    # tb_writer = SummaryWriter(logdir = logdir)
+    logdir = os.path.join(args.logdir, exp_name)
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
 
     dataset = load_dataset(args.text_dataset, cache_dir = args.cache_dir)
 
@@ -84,28 +77,26 @@ def main():
     y_test = dataset['test']['label']
 
 
-    classifiers = [
-        LogisticRegression( verbose = 2, max_iter = 1000, n_jobs = -1, solver = "saga"),
-        Perceptron( verbose = 2),
-        LinearSVC(verbose = 2),
-        SGDClassifier(n_jobs = -1, verbose =2)
-        ]
+    classifiers = {
+        "logistic" : LogisticRegression( verbose = 2, max_iter = 1000, n_jobs = -1, solver = "saga"),
+        "perceptron" : Perceptron( verbose = 2),
+        "svc" : LinearSVC(verbose = 2),
+        "sgd" : SGDClassifier(n_jobs = -1, verbose =2)
+    }
 
-    classifier = classifiers[int(args.Algorithm)]
-    
+    classifier = classifiers[args.algorithm]
     classifier.fit(X_train, y_train)
-    Score = classifier.score(X_test, y_test)
+    score = classifier.score(X_test, y_test)
 
-    ResultText = "Dataset:", args.text_dataset, "\t\tClassifier:", classifier, "\t\tScore:", Score
-    SavePath = MLModelFiles + "yelp_polarity"+  "_" + 'SGDClassifier'+ 'Model'
+    result_text = "Dataset:", args.text_dataset, "\t\tClassifier:", classifier, "\t\tScore:", score
     
     print('-------------------------------------------------')
-    print(ResultText)
+    print(result_text)
     print('-------------------------------------------------')
-    # tb_writer.add_text('BaselineModel', ResultText, 0)
 
-    print("Model Saved as:", SavePath+'.pkl')
-    save_obj(classifier, SavePath)
+    model_path = os.path.join(logdir, "model.pkl")
+    save_obj(classifier, model_path)
+    print("Model Saved as:", model_path)
 
 if __name__ == '__main__':
     main()
